3952bdc5fd124b985806aae72235e928545de461
persi2711 <a361193@uabc.edu.mx>
2026-03-02 03:17:53 -0800

    Auto migrations

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,6 +2,22 @@
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
 import { join } from 'path';
+import { User } from './modules/core/objects/user-data-module/users/users.entity';
+import { MediaSocial } from './modules/core/objects/user-data-module/media-socials/media-social.entity';
+import { LifeWay } from './modules/core/objects/user-data-module/life-ways/life-ways.entity';
+import { GeneralInfo } from './modules/core/objects/user-data-module/general-Infos/general-info.entity';
+import { ContactInfo } from './modules/core/objects/user-data-module/contact-Infos/contact-info.entity';
+import { Account } from './modules/core/objects/user-data-module/accounts/account.entity';
+import { Vote } from './modules/core/objects/events-data-module/votes/votes.entity';
+import { TextEvent } from './modules/core/objects/events-data-module/texts-events/text-event.entinty';
+import { Result } from './modules/core/objects/events-data-module/results/result.entity';
+import { Postulation } from './modules/core/objects/events-data-module/postulations/postulation.entity';
+import { Event } from './modules/core/objects/events-data-module/events/events.entity';
+import { EventParticipant } from './modules/core/objects/events-data-module/event-partipants/event-participants.entity';
+import { AskOption } from './modules/core/objects/events-data-module/asks-options/asks-options.entity';
+import { Answer } from './modules/core/objects/events-data-module/answers/answers.entity';
+import { Resource } from './modules/core/objects/application-data-module/resources/resource.entity';
+import { EmailVerificationToken } from './modules/core/objects/application-data-module/auth/email-verification-tokens';
 
 config(); // Carga el .env para desarrollo local
 
@@ -14,7 +30,24 @@ export const AppDataSource = new DataSource({
   database: process.env.DB_NAME,
 
   // Usamos join para normalizar las rutas según el sistema operativo (Linux en Docker)
-  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+  entities: [
+    User,
+    MediaSocial,
+    LifeWay,
+    GeneralInfo,
+    ContactInfo,
+    Account,
+    Vote,
+    TextEvent,
+    Result,
+    Postulation,
+    Event,
+    EventParticipant,
+    AskOption,
+    Answer,
+    Resource,
+    EmailVerificationToken,
+  ],
   migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
 
   synchronize: false,
